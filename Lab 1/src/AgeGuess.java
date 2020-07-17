205b243df01f898fc32ae38235a90310cccd3ce8
bhavanakota <bhavana.kota@gmail.com>
2020-07-17 14:04:14 -0500

    First Commit
    Upload all files

---
+++ Lab 1/src/AgeGuess.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class AgeGuess {
+
+	public static void main(String[] args) {
+		String name;
+		int ageGuess;
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("Enter the name: ");
+		name = scan.next();
+		
+		System.out.print("Enter the age: ");
+		ageGuess = scan.nextInt();
+		
+
+
+	}
+
+}
